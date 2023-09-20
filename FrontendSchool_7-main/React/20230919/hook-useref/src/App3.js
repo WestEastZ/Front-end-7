03ba1314efb195ac3558228efcfb910af38d9ceb
WestEastZ <tjehdwn10110@naver.com>
2023-09-20 16:09:33 +0900

    20230920

--- FrontendSchool_7-main/React/20230919/hook-useref/src/App3.js
+++ FrontendSchool_7-main/React/20230919/hook-useref/src/App3.js
@@ -1,21 +1,27 @@
 import React from "react";
 
-// let guest = 0;
+let guest = 0;
 
-function Cup({ guest }) {
-  // guest = guest + 1;
+function Cup() {
+  guest = guest + 1;
   return <h2>{guest}번 컵</h2>;
 }
 
 export default function App3() {
   return (
     <div>
-      <Cup guest={1} />
-      <Cup guest={2} />
+      <Cup />
+      <Cup />
+      <Cup />
+      <Cup />
+      <Cup />
+      <Cup />
+      <Cup />
+      {/* <Cup guest={2} />
       <Cup guest={3} />
       <Cup guest={4} />
       <Cup guest={5} />
-      <Cup guest={6} />
+      <Cup guest={6} /> */}
     </div>
   );
 }
--- FrontendSchool_7-main/React/20230920/hook-usememo/src/index.js
+++ FrontendSchool_7-main/React/20230920/hook-usememo/src/index.js
@@ -3,8 +3,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import App2 from "./App2";
-import App3 from "./App3";
+import App3 from './App3';
+import App4 from "./App4";
+import { DarkMode } from "./DarkMode";
+import ClassLifeCycle from "./ClassLifeCycle";
+import Wrap from "./HookLifeCycle";
+import LayoutEffect from "./LayoutEffect";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App3 />);
+root.render(<LayoutEffect />);
